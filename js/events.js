c5afc554d4353bc0bda89b33c75518d8468c7c2c
ApurboShib <shibjoy088@gmail.com>
2025-01-15 23:46:54 +0600

    try all possible approch

--- js/events.js
+++ js/events.js
@@ -1,1 +1,15 @@
 console.log("lets dhamaka with doms");
+
+function makeRed() {
+    document.body.style.backgroundColor='red';
+}
+
+// option three.
+
+const makeElebtn = document.getElementById('make-blue');
+    // console.log(makeElebtn);
+    makeElebtn.onclick = makeBlue;
+
+     function makeBlue() {
+        document.body.style.backgroundColor = 'blue';
+    }
